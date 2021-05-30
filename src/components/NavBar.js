9e8d2b5a4a49b1239c85104e525eee2d3bf1d25a
cristofer <jaimez07788@gmail.com>
2021-05-30 17:21:07 -0500

    NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,47 +1,37 @@
-import React, { Component } from "react";
-import fire from ".././firebase";
+import React from "react";
 
-class NavBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-    };
-  }
+function NavBar() {
+  return (
+    <div>
+      <nav>
+        <div className="nav-wrapper   teal darken-3 ">
+          <a href="#!" className="brand-logo right">
+            {this.props.email ? (
+              <button
+                className="btn-floating small red"
+                onClick={() => {
+                  fire.auth().signOut();
+                }}
+              >
+                logOut
+              </button>
+            ) : (
+              <i className="material-icons">account_circle</i>
+            )}
+          </a>
 
-  render() {
-    return (
-      <React.StrictMode>
-        <nav>
-          <div className="nav-wrapper   teal darken-3 ">
-            <a href="#!" className="brand-logo right">
-              {this.props.email ? (
-                <button
-                  className="btn-floating small red"
-                  onClick={() => {
-                    fire.auth().signOut();
-                  }}
-                >
-                  logOut
-                </button>
-              ) : (
+          <ul id="" className="">
+            <li>
+              {" "}
+              <a href="#!">
                 <i className="material-icons">account_circle</i>
-              )}
-            </a>
-
-            <ul id="" className="">
-              <li>
-                {" "}
-                <a href="#!">
-                  <i className="material-icons">account_circle</i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </React.StrictMode>
-    );
-  }
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
