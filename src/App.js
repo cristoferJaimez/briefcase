bf18586a2b9a479db789c0622c33e15f7ce7fa0d
cristofer <jaimez07788@gmail.com>
2021-05-30 16:24:11 -0500

    presentacion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 //CSS
 import "./App.css";
 //Components
@@ -8,21 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DataDev></DataDev>
       </header>
-
-      <DataDev></DataDev>
     </div>
   );
 }
